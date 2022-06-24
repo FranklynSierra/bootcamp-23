b34cd207d104f3450b1b288062093adb281a54eb
fran <franklynssierracontreras@gmail.com>
2022-06-24 13:09:33 -0400

    proyecto 23 de openBootcamp

---
+++ depuracion.js
@@ -0,0 +1,11 @@
+function GenerateFibonacci(num){
+    var fibonacci = [];
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+    for (var i = 2; i < num; i++) {
+      fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+    }
+    return fibonacci;
+    }
+    var f = GenerateFibonacci(10);
+    console.log(f)
